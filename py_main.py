778c33794096808823861730b810e8b61064b260
JeanCao233 <jeancao233@gmail.com>
2024-03-21 22:01:36 -0400

    reorganized main python script and updated data processing method to account for gravity

---
+++ py_main.py
@@ -0,0 +1,214 @@
+import serial
+import time
+import timeit
+import serial.tools.list_ports
+import logging
+import binascii
+import numpy as np
+import matplotlib.pyplot as plt
+from EKF3d import EKF_SLAM
+
+
+ports = list(serial.tools.list_ports.comports())
+
+'''
+for p in ports:
+    print("======")
+    print(p)
+'''
+
+'''
+def write_read(x):
+    arduino.write(bytes(x,   'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return   data
+
+while True:
+    num = input("Enter a number: ")
+    value   = write_read(num)
+    print(value)
+'''
+
+
+def create_log():
+    # Configure logging
+    logging.basicConfig(filename='serial.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+    arduino = serial.Serial(port='/dev/cu.usbmodem1101',   baudrate=9600, timeout=.1)
+
+    with open('sensor_data.log', 'w') as log_file:
+        try:
+            while True:
+                try:
+                    # Read data from serial port
+                    data = arduino.readline().decode().strip()
+                    print(data)
+                    values = data.split(' ')
+                    if not data.strip():  # Check if the line is empty or contains only whitespace
+                        print("Empty line, skipping...")
+                        continue
+                    #values = data.split(',')  # Split the line into individual values
+                    
+
+                except UnicodeDecodeError:
+                    # Handle decoding errors
+                    print("Decoding error, skipping line...")
+                    continue
+
+                # Write data to log file
+                log_file.write(f"{values[0]} {values[1]} {values[2]}\n")
+                log_file.flush()  # Flush buffer to ensure data is written immediately
+
+                # Print data to console (optional)
+                print(data)
+        except KeyboardInterrupt:
+            arduino.close()
+
+
+def calcD_fromLog(datafile, sampling_freq):
+    sampling_frequency = 100
+
+    dt = 1.0 / sampling_frequency
+
+    # read file with ax, ay, az, wx, wy, wz measurements from IMU
+    imu_data = np.genfromtxt(datafile, delimiter=' ')
+
+    ax = imu_data[:, 0]
+    ay = imu_data[:, 1]
+    az = imu_data[:, 2]
+
+    l = len(ax)
+    vx = np.zeros(l - 1)
+    vy = np.zeros(l - 1)
+    vz = np.zeros(l - 1)
+    for i1 in range(l - 1):
+        vx[i1] = (ax[i1+1] + ax[i1])*dt/2
+        vy[i1] = (ay[i1+1] + ay[i1])*dt/2
+        vz[i1] = (az[i1+1] + az[i1])*dt/2
+    
+    dx = np.zeros(l - 2)
+    dy = np.zeros(l - 2)
+    dz = np.zeros(l - 2)
+    x = np.zeros(l - 1)
+    y = np.zeros(l - 1)
+    z = np.zeros(l - 1)
+    for i2 in range(l - 2):
+        dx[i2] = (vx[i2+1] + vx[i2])*dt/2
+        dy[i2] = (vy[i2+1] + vy[i2])*dt/2
+        dz[i2] = (vz[i2+1] + vz[i2])*dt/2
+        x[i2+1] = x[i2] + dx[i2]
+        y[i2+1] = y[i2] + dy[i2]
+        z[i2+1] = z[i2] + dz[i2]
+    
+    fig = plt.figure(1)
+    ax1 = plt.subplot(aspect='equal', projection='3d')
+    ax1.plot(x, y, z, 'b--')
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
+    plt.show()
+    return x, y, z
+
+def runEKFtest():
+    start = timeit.default_timer()
+    
+    m3d = np.array([[0.,  0.,  0.],
+                    [0.,  20., 10.],
+                    [-10., 100.,  -10.],
+                    [20., 200.,  70.],
+                    [0,  -200,  -20.],
+                    [-20, 0,  30.],
+                    [10, -70, -70.],
+                    [-50, -100, -60.]]).reshape(-1)
+    
+    '''
+    minX, maxX, minY, maxY = -120., 450., -500., 50.
+    map_x = np.linspace(minX, maxX, 7)
+    map_y = np.linspace(minY, maxY, 7)
+    map_X, map_Y = np.meshgrid(map_x, map_y)
+    map_X = map_X.reshape(-1,1)
+    map_Y = map_Y.reshape(-1,1)
+    m = np.hstack((map_X, map_Y)).reshape((-1))
+    '''
+
+    dt = 0.01
+    T = np.arange(0, 10, dt)
+    n = int(len(m3d)/3)
+    W = np.zeros((6+3*n, 6+3*n))                    #xyz + rpy + 3m
+    W[0:6, 0:6] = dt**2 * 1 * np.eye(6)
+    V = 0.1*np.eye(4*n)
+    V[n:,n:] = 0.01*np.eye(3*n)                     #distance*n, theta*n, beta*n, gamma*n
+
+    # EKF estimation
+    mu_ekf = np.zeros((6+3*n, len(T)))
+    mu_ekf[0:6,0] = np.array([-20.2, -221.8, -52.1, 0.1, 0.2, -0.1])
+    # mu_ekf[3:,0] = m + 0.1
+    mu_ekf[6:,0] = m3d + np.random.multivariate_normal(np.zeros(3*n), 0.5*np.eye(3*n))
+    init_P = 1*np.eye(6+3*n)
+
+    # initialize EKF SLAM
+    slam = EKF_SLAM(mu_ekf[:,0], init_P, dt, W, V, n)
+    
+    # real state
+    mu = np.zeros((6+3*n, len(T)))
+    mu[0:6,0] = np.array([-20, -220, -50, 0, 0, 0])
+    mu[6:,0] = m3d
+
+    y_hist = np.zeros((4*n, len(T)))
+
+    for i, t in enumerate(T):
+        if i > 0:
+            # real dynamics
+            u = [-50*np.cos(t*3), 50, -100*np.sin(t*3), 1*np.sin(t*3), 1*np.sin(t*3), 1*np.sin(t*3)]
+            # u = [0.5, 0.5*np.sin(t*0.5), 0]
+            # u = [0.5, 0.5, 0]
+            mu[:,i] = slam._f(mu[:,i-1], u) + \
+                np.random.multivariate_normal(np.zeros(6+3*n), W)
+            
+            # measurements
+            y = slam._h(mu[:,i]) + np.random.multivariate_normal(np.zeros(4*n), V)
+
+            y_hist[:,i] = (y-slam._h(slam.mu))
+
+            # apply EKF SLAM
+            mu_est, _ = slam.predict_and_correct(y, u)
+            mu_ekf[:,i] = mu_est
+
+            #print("True      X, Y, psi:", mu[0:3,i])
+            #print("Estimated X, Y, psi:", mu_est[0], mu_est[1], mu_est[2])
+            #print("-------------------------------------------------------")
+            
+            #if i == 50: break
+
+
+    fig = plt.figure(1)
+    ax1 = plt.subplot(aspect='equal', projection='3d')
+    ax1.plot(mu[0,:], mu[1,:], mu[2,:], 'b')
+    ax1.plot(mu_ekf[0,:], mu_ekf[1,:], mu_ekf[2,:], 'r--')
+    mf = m3d.reshape((-1,3))
+    ax1.scatter(mf[:,0], mf[:,1], mf[:,2])
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
+
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)
+
+    plt.show()
+
+if __name__ == '__main__':
+
+    """
+    Read from serial
+    """
+    #create_log()
+
+    """
+    Calculate step displacement
+    """
+    #x, y, z = calcD_fromLog('sensor_data.log', sampling_freq=10)
+
+    """
+    Run EKF test on randomly generated test
+    """
+    runEKFtest()
--- testCode/testGyro/testGyro.ino
+++ testCode/testGyro/testGyro.ino
@@ -2,12 +2,18 @@
 
 #include "Wire.h" // This library allows you to communicate with I2C devices.
 #include "filters.h"
+#include <MPU6050.h>
+#include <MadgwickAHRS.h>
 
-const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+MPU6050 mpu;
 
-int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
-int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
-int16_t temperature; // variables for temperature data
+//const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+
+int16_t ax, ay, az, gx, gy, gz;
+float accelX, accelY, accelZ, gyroX, gyroY, gyroZ;
+float prev_roll=0.0, prev_pitch=0.0, prev_yaw=0.0;
+float roll, pitch, yaw;
+float delta_roll, delta_pitch, delta_yaw;
 
 char tmp_str[7]; // temporary variable used in convert function
 
@@ -17,64 +23,126 @@ char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, r
 }
 
 // Parameters for filter
-const float cutoff_freq   = 20.0;  //Cutoff frequency in Hz
+const float cutoff_freq   = 10.0;  //Cutoff frequency in Hz
 const float sampling_time = 0.005; //Sampling time in seconds.
 IIR::ORDER  order  = IIR::ORDER::OD3; // Order (OD1 to OD4)
     
 // Creating a low-pass filter class
-Filter f(cutoff_freq, sampling_time, order);
+Filter f_ax(cutoff_freq, sampling_time, order);
+Filter f_ay(cutoff_freq, sampling_time, order);
+Filter f_az(cutoff_freq, sampling_time, order);
+Filter f_gx(cutoff_freq, sampling_time, order);
+Filter f_gy(cutoff_freq, sampling_time, order);
+Filter f_gz(cutoff_freq, sampling_time, order);
+Filter f_ax_(cutoff_freq, sampling_time, order);
+Filter f_ay_(cutoff_freq, sampling_time, order);
+Filter f_az_(cutoff_freq, sampling_time, order);
+
+// Creating a high-pass filter class
+Madgwick filter;
 
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
-  Wire.write(0x6B); // PWR_MGMT_1 register
-  Wire.write(0); // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
+  mpu.initialize();
+  // Set accelerometer range to ±2g
+  //mpu.setFullScaleAccelRange(AFS_SEL=0);
+
+  //Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  //Wire.write(0x6B); // PWR_MGMT_1 register
+  //Wire.write(0); // set to zero (wakes up the MPU-6050)
+  //Wire.endTransmission(true);
   
   // Setting up sensor filters
   pinMode(A0, INPUT);
   // Enable pull-ups if necessary
   digitalWrite(A0, HIGH);
 
+  filter.begin(10);
+
 }
 
 void loop() {
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
-  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
-  
-  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
-  
-  // print out data
-  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
-  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
-  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
-  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
-  Serial.print(" | tmp = "); Serial.print(temperature);
-  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
-  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
-  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
-  Serial.println();
 
-  // Filtering input signal
-  float filtered_gX = f.filterIn(gyro_x);
-  float filtered_gY = f.filterIn(gyro_y);
-  float filtered_gZ = f.filterIn(gyro_z);
-  // print out filtered data
-  Serial.print(" | filtered_gX = "); Serial.print(filtered_gX);
-  Serial.print(" | filtered_gY = "); Serial.print(filtered_gY);
-  Serial.print(" | filtered_gZ = "); Serial.print(filtered_gZ);
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  
+  accelX = f_ax.filterIn(ax)/16384.0;
+  accelY = f_ay.filterIn(ay)/16384.0;
+  accelZ = f_az.filterIn(az)/16384.0;
+  gyroX = f_gx.filterIn(gx)/131.0;
+  gyroY = f_gy.filterIn(gy)/131.0;
+  gyroZ = f_gz.filterIn(gz)/131.0;
+
+  
+  //Serial.print(" | filtered_gX = "); Serial.print(filtered_gX);
+  //Serial.print(" | filtered_gY = "); Serial.print(filtered_gY);
+  //Serial.print(" | filtered_gZ = "); Serial.print(filtered_gZ);
+  
+
+  // Update filter with sensor data
+  filter.updateIMU(gyroX, gyroY, gyroZ, accelX, accelY, accelZ);
+
+  // Get orientation quaternion from filter
+  roll = filter.getRoll();
+  pitch = filter.getPitch();
+  yaw = filter.getYaw() - 180;
+  delta_roll = roll - prev_roll;
+  delta_pitch = pitch - prev_pitch;
+  delta_yaw = yaw - prev_yaw;
+  prev_roll = roll;
+  prev_pitch = pitch;
+  prev_yaw = yaw;
+
+  // Calculate linear acceleration without gravity
+  float accelWithoutGravityX = f_ax_.filterIn(accelX - (-sin(pitch/180.0*PI)))*(-9.81);
+  float accelWithoutGravityY = f_ay_.filterIn(accelY - (sin(roll/180.0*PI) * cos(pitch/180.0*PI)))*(-9.81);
+  float accelWithoutGravityZ = f_az_.filterIn(accelZ - (cos(roll/180.0*PI) * cos(pitch/180.0*PI)))*(-9.81);
+  
+  
+  // print filtered data
+  //printScaledData(accelX, accelY, accelZ, gyroX, gyroY, gyroZ);
+  
+  // print accelerations without gravity
+  //printAccelerationWithoutG(accelWithoutGravityX, accelWithoutGravityY, accelWithoutGravityZ, roll, pitch, yaw);
+
+  // print output
+  printOutput(accelWithoutGravityX, accelWithoutGravityY, accelWithoutGravityZ);
   
   // delay
-  delay(1000);
+  delay(100);
+}
+
+void printScaledData(float accelX, float accelY, float accelZ, float gyroX, float gyroY, float gyroZ) {
+  //Serial.print("Acceleration: ");
+  // acceleration in g
+  Serial.print(accelX);
+  Serial.print(" ");
+  Serial.print(accelY);
+  Serial.print(" ");
+  Serial.print(accelZ);
+  Serial.print(" ");
+  // angle in degrees
+  Serial.print(gyroX);
+  Serial.print(" ");
+  Serial.print(gyroY);
+  Serial.print(" ");
+  Serial.println(gyroZ);
+}
+
+void printAccelerationWithoutG(float accelWithoutGravityX, float accelWithoutGravityY, float accelWithoutGravityZ, float roll, float pitch, float yaw) {
+  Serial.print("Accel X: "); Serial.print(accelWithoutGravityX); Serial.print("\t");
+  Serial.print("Accel Y: "); Serial.print(accelWithoutGravityY); Serial.print("\t");
+  Serial.print("Accel Z: "); Serial.print(accelWithoutGravityZ); Serial.print("\t");
+  Serial.print("roll: "); Serial.print(roll); Serial.print("\t");
+  Serial.print("pitch: "); Serial.print(pitch); Serial.print("\t");
+  Serial.print("yaw: "); Serial.println(yaw);
+}
+
+void printOutput(float accelWithoutGravityX, float accelWithoutGravityY, float accelWithoutGravityZ) {
+  Serial.print(accelWithoutGravityX);
+  Serial.print(" ");
+  Serial.print(accelWithoutGravityY);
+  Serial.print(" ");
+  Serial.println(accelWithoutGravityZ);
 }
